0c230347ada8de1c5014a2d7488b0515b5d4f5a0
NaveenSidhartha <naveensidhartha@gmail.com>
2024-09-10 14:47:33 +0530

    directorymain (#17)
    * added directorymain primitive
    
    * typo fix
    
    ---------
    
    Co-authored-by: kn@cix.ie <kn@cix.ie>

--- README.md
+++ README.md
@@ -1,48 +1,10 @@
 # Primitives
 
-## bridge_podnet
-Primitive for Building Public subnet bridge on PodNet using netplan service.
+## direcotymain
+Primitive to Build and Delete directories on PodNet HA
 
-Supported verbs:
+supported verbs:
 
-- head 
-    - identifier: str
-    - config_filepath=None
-    
-- build
-    - address_range: str
-    - identifier: str
-    - config_filepath=None
-
-## firewallns
-Primitive for setup of Namepace Firewall on PodNet using nftable service.
-
-Supported verbs:
-
-- build_overwrite
-    - namespace_identifier: str
-    - config_filepath=None
-    - firewall_rules=None
-    - log=None
-    - nat=None
-    - namespace_pubif=None
-    - namespace_pubif6=None
-
-## network_namespace
-Primitive for Namespace Network on PodNet.
-
-Supported verbs:
-
-- build
-    - namespace_identifier: str
-    - bridge_podnet_identifier=None
-    - bridge6_podnet_identifier=None
-    - config_filepath=None
-    - ip4=None
-    - ip6=None
-    - namespace_networks=None
-
-- scrub
-    - namespace_identifier: str
-    - config_filepath=None
- 
+- build:
+    - path: str
+    - config_file=None
---
+++ directorymain.py
@@ -0,0 +1,140 @@
+"""
+Primitive to Build and Delete directories on PodNet HA
+"""
+
+# stdlib
+import json
+import ipaddress
+from pathlib import Path
+from typing import Tuple
+# lib
+from cloudcix.rcc import comms_ssh, CouldNotConnectException
+# local
+
+
+__all__ = [
+    'build',
+]
+
+SUCCESS_CODE = 0
+
+
+def build(
+        path: str,
+        config_file=None,
+) -> Tuple[bool, str]:
+    """
+    description:
+        Creates directory on PodNet HA.
+
+    parameters:
+        path:
+            description: The path to be created on the PodNet
+            type: string
+            required: true
+        config_file:
+            description: The path to the config.json file
+            type: string
+            required: false
+    return:
+        description: |
+            A tuple with a boolean flag stating the build was successful or not and
+            the output or error message.
+        type: tuple
+    """
+    # Define message
+    messages = {
+        1000: f'Successfully created directory {path}',
+        1031: f'Successfully created directory {path} on enabled PodNet but Failed to connect to the disabled PodNet '
+              f'from the config file {config_file}',
+        1032: f'Successfully created directory {path} on enabled PodNet but Failed to create on the disabled PodNet',
+
+        3000: f'Failed to create directory {path}',
+        3011: f'Failed to load config file {config_file}, It does not exits.',
+        3012: f'Failed to get `ipv6_subnet` from config file {config_file}',
+        3013: f'Invalid value for `ipv6_subnet` from config file {config_file}',
+        3014: f'Failed to get `podnet_a_enabled` from config file {config_file}',
+        3015: f'Failed to get `podnet_b_enabled` from config file {config_file}',
+        3016: f'Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, both are True',
+        3017: f'Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, both are False',
+        3018: f'Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, one or both are non booleans',
+        3021: f'Failed to connect to the enabled PodNet from the config file {config_file}',
+        3022: f'Failed to create directory {path} on the enabled PodNet'
+    }
+
+    # Default config_file if it is None
+    if config_file is None:
+        config_file = '/etc/cloudcix/pod/configs/config.json'
+
+    # Get load config from config_file
+    if not Path(config_file).exists():
+        return False, messages[3011]
+    with Path(config_file).open('r') as file:
+        config = json.load(file)
+
+    # Get the ipv6_subnet from config_file
+    ipv6_subnet = config.get('ipv6_subnet', None)
+    if ipv6_subnet is None:
+        return False, messages[3012]
+    # Verify the ipv6_subnet value
+    try:
+        ipaddress.ip_network(ipv6_subnet)
+    except ValueError:
+        return False, messages[3013]
+
+    # Get the PodNet Mgmt ips from ipv6_subnet
+    podnet_a = f'{ipv6_subnet.split("/")[0]}10:0:2'
+    podnet_b = f'{ipv6_subnet.split("/")[0]}10:0:3'
+
+    # Get `podnet_a_enabled` and `podnet_b_enabled`
+    podnet_a_enabled = config.get('podnet_a_enabled', None)
+    if podnet_a_enabled is None:
+        return False, messages[3014]
+    podnet_b_enabled = config.get('podnet_b_enabled', None)
+    if podnet_a_enabled is None:
+        return False, messages[3015]
+
+    # First run on enabled PodNet
+    if podnet_a_enabled is True and podnet_b_enabled is False:
+        enabled = podnet_a
+        disabled = podnet_b
+    elif podnet_a_enabled is False and podnet_b_enabled is True:
+        enabled = podnet_b
+        disabled = podnet_a
+    elif podnet_a_enabled is True and podnet_b_enabled is True:
+        return False, messages[3016]
+    elif podnet_a_enabled is False and podnet_b_enabled is False:
+        return False, messages[3017]
+    else:
+        return False, messages[3018]
+
+    # define payload
+    payload = f'mkdir --parents {path}'
+
+    # call rcc comms_ssh on enabled PodNet
+    try:
+        create_dir, stdout, stderr = comms_ssh(
+            host_ip=enabled,
+            payload=payload,
+            username='robot',
+        )
+    except CouldNotConnectException:
+        return False, messages[3021]
+
+    if create_dir.exit_code != SUCCESS_CODE:
+        return False, messages[3022]
+
+    # call rcc comms_ssh on disabled PodNet
+    try:
+        create_dir, stdout, stderr = comms_ssh(
+            host_ip=disabled,
+            payload=payload,
+            username='robot',
+        )
+    except CouldNotConnectException:
+        return True, messages[1031]
+
+    if create_dir.exit_code != SUCCESS_CODE:
+        return True, messages[1032]
+
+    return True, messages[1000]
