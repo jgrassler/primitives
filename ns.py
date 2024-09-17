25b69c6b644e5991c6434dba5f555126e494ba1b
Johannes Grassler <info@computer-grassler.de>
2024-09-17 10:40:26 +0200

    EXPERIMENTAL: move config processing to utils.py.

--- ns.py
+++ ns.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from typing import Tuple
 # lib
 from cloudcix.rcc import comms_ssh, CHANNEL_SUCCESS, VALIDATION_ERROR, CONNECTION_ERROR
+from utils import load_pod_config
 # local
 
 
@@ -57,15 +58,6 @@ def build(
     messages = {
         1000: f'1000: Successfully created network name space {name} on both PodNet nodes.',
         1001: f'1001: Success: name space {name} exists already',
-        2111: f'2011: Config file {config_file} loaded.',
-        3011: f'3011: Failed to load config file {config_file}, It does not exist.',
-        3012: f'3012: Failed to get `ipv6_subnet` from config file {config_file}',
-        3013: f'3013: Invalid value for `ipv6_subnet` from config file {config_file}',
-        3014: f'3014: Failed to get `podnet_a_enabled` from config file {config_file}',
-        3015: f'3015: Failed to get `podnet_b_enabled` from config file {config_file}',
-        3016: f'3016: Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, both are True',
-        3017: f'3017: Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, both are False',
-        3018: f'3018: Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, one or both are non booleans',
         3021: f'3021: Failed to connect to the enabled PodNet from the config file {config_file} for find_namespace_payload: ',
         3022: f'3022: Failed to connect to the enabled PodNet from the config file {config_file} for create_namespace_payload: ',
         3023: f'3023: Failed to create name space {name} on the enabled PodNet. Payload exited with status ',
@@ -109,51 +101,16 @@ def build(
         3067: f'3067: Failed to run enable_lo1_payload on the disabled PodNet from the config file {config_file}. Payload exited with status ',
     }
 
-    # Default config_file if it is None
-    if config_file is None:
-        config_file = '/opt/robot/config.json'
-
-    # Get load config from config_file
-    if not Path(config_file).exists():
-        return False, messages[3011]
-    with Path(config_file).open('r') as file:
-        config = json.load(file)
-
-    # Get the ipv6_subnet from config_file
-    ipv6_subnet = config.get('ipv6_subnet', None)
-    if ipv6_subnet is None:
-        return False, messages[3012]
-    # Verify the ipv6_subnet value
-    try:
-        ipaddress.ip_network(ipv6_subnet)
-    except ValueError:
-        return False, messages[3013]
-
-    # Get the PodNet Mgmt ips from ipv6_subnet
-    podnet_a = f'{ipv6_subnet.split("/")[0]}10:0:2'
-    podnet_b = f'{ipv6_subnet.split("/")[0]}10:0:3'
-
-    # Get `podnet_a_enabled` and `podnet_b_enabled`
-    podnet_a_enabled = config.get('podnet_a_enabled', None)
-    if podnet_a_enabled is None:
-        return False, messages[3014]
-    podnet_b_enabled = config.get('podnet_b_enabled', None)
-    if podnet_a_enabled is None:
-        return False, messages[3015]
-
-    # First run on enabled PodNet
-    if podnet_a_enabled is True and podnet_b_enabled is False:
-        enabled = podnet_a
-        disabled = podnet_b
-    elif podnet_a_enabled is False and podnet_b_enabled is True:
-        enabled = podnet_b
-        disabled = podnet_a
-    elif podnet_a_enabled is True and podnet_b_enabled is True:
-        return False, messages[3016]
-    elif podnet_a_enabled is False and podnet_b_enabled is False:
-        return False, messages[3017]
-    else:
-        return False, messages[3018]
+    status, config_data, msg = load_pod_config(config_file)
+    if not status:
+      if config_data['raw'] is None:
+          return False, msg
+      else:
+          return False, msg + "\nJSON dump of raw configuration:\n" + json.dumps(config_data['raw'],
+              indent=2,
+              sort_keys=True)
+    enabled = config_data['processed']['enabled']
+    disabled = config_data['processed']['enabled']
 
     name_grepsafe = name.replace('.', '\.')
     lo_addr_grepsafe = lo_addr.replace('.', '\.')
@@ -454,15 +411,6 @@ def scrub(
 
     messages = {
         1100: f'1100: Successfully removed name space {name} from both PodNet nodes.',
-        2111: f'2111: Config file {config_file} loaded.',
-        3111: f'3111: Failed to load config file {config_file}, It does not exist.',
-        3112: f'3112: Failed to get `ipv6_subnet` from config file {config_file}',
-        3113: f'3113: Invalid value for `ipv6_subnet` from config file {config_file}',
-        3114: f'3114: Failed to get `podnet_a_enabled` from config file {config_file}',
-        3115: f'3115: Failed to get `podnet_b_enabled` from config file {config_file}',
-        3116: f'3116: Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, both are True',
-        3117: f'3117: Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, both are False',
-        3118: f'3118: Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, one or both are non booleans',
         3121: f'3121: Failed to connect to the enabled PodNet from the config file {config_file} for find_namespace_payload: ',
         3122: f'3122: Failed to connect to the enabled PodNet from the config file {config_file} for delete_namespace_payload: ',
         3123: f'3123: Failed to delete name space {name} on the enabled PodNet. Payload exited with status ',
@@ -474,51 +422,16 @@ def scrub(
                'Payload exited with status ',
     }
 
-    # Default config_file if it is None
-    if config_file is None:
-        config_file = '/opt/robot/config.json'
-
-    # Get load config from config_file
-    if not Path(config_file).exists():
-        return False, messages[3111]
-    with Path(config_file).open('r') as file:
-        config = json.load(file)
-
-    # Get the ipv6_subnet from config_file
-    ipv6_subnet = config.get('ipv6_subnet', None)
-    if ipv6_subnet is None:
-        return False, messages[3112]
-    # Verify the ipv6_subnet value
-    try:
-        ipaddress.ip_network(ipv6_subnet)
-    except ValueError:
-        return False, messages[3113]
-
-    # Get the PodNet Mgmt ips from ipv6_subnet
-    podnet_a = f'{ipv6_subnet.split("/")[0]}10:0:2'
-    podnet_b = f'{ipv6_subnet.split("/")[0]}10:0:3'
-
-    # Get `podnet_a_enabled` and `podnet_b_enabled`
-    podnet_a_enabled = config.get('podnet_a_enabled', None)
-    if podnet_a_enabled is None:
-        return False, messages[3114]
-    podnet_b_enabled = config.get('podnet_b_enabled', None)
-    if podnet_a_enabled is None:
-        return False, messages[3115]
-
-    # First run on enabled PodNet
-    if podnet_a_enabled is True and podnet_b_enabled is False:
-        enabled = podnet_a
-        disabled = podnet_b
-    elif podnet_a_enabled is False and podnet_b_enabled is True:
-        enabled = podnet_b
-        disabled = podnet_a
-    elif podnet_a_enabled is True and podnet_b_enabled is True:
-        return False, messages[3116]
-    elif podnet_a_enabled is False and podnet_b_enabled is False:
-        return False, messages[3117]
-    else:
-        return False, messages[3118]
+    status, config_data, msg = load_pod_config(config_file)
+    if not status:
+      if config_data['raw'] is None:
+          return False, msg
+      else:
+          return False, msg + "\nJSON dump of raw configuration:\n" + json.dumps(config_data['raw'],
+              indent=2,
+              sort_keys=True)
+    enabled = config_data['processed']['enabled']
+    disabled = config_data['processed']['enabled']
 
     name_grepsafe = name.replace('.', '\.')
 
@@ -640,15 +553,6 @@ def read(
 
     messages = {
         1200: f'1200: Successfully retrieved network name space {name} status from both PodNet nodes.',
-        2211: f'2211: Config file {config_file} loaded.',
-        3211: f'3211: Failed to load config file {config_file}, It does not exist.',
-        3212: f'3212: Failed to get `ipv6_subnet` from config file {config_file}',
-        3213: f'3213: Invalid value for `ipv6_subnet` from config file {config_file}',
-        3214: f'3214: Failed to get `podnet_a_enabled` from config file {config_file}',
-        3215: f'3215: Failed to get `podnet_b_enabled` from config file {config_file}',
-        3216: f'3216: Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, both are True',
-        3217: f'3217: Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, both are False',
-        3218: f'3218: Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, one or both are non booleans',
         3221: f'3221: Failed to connect to the enabled PodNet from the config file {config_file} for find_namespace_payload: ',
         3222: f'3222: Failed to find name space {name} on the enabled PodNet. Payload exited with status ',
         3223: f'3223: Failed to connect to the enabled PodNet from the config file {config_file} for find_forwardv4_payload',
@@ -671,71 +575,16 @@ def read(
     data_dict = None
     message_list = ()
 
-    # Default config_file if it is None
-    if config_file is None:
-        config_file = '/opt/robot/config.json'
-
-    # Get load config from config_file
-    if not Path(config_file).exists():
-        retval = False
-        message_list.append(messages[3211])
-    with Path(config_file).open('r') as file:
-        config = json.load(file)
-
-    # Get the ipv6_subnet from config_file
-    ipv6_subnet = config.get('ipv6_subnet', None)
-    if ipv6_subnet is None:
-        retval = False
-        message_list.append(messages[3212])
-    # Verify the ipv6_subnet value
-    try:
-        ipaddress.ip_network(ipv6_subnet)
-    except ValueError:
-        retval = False
-        message_list.append(messages[3213])
-
-    # Get the PodNet Mgmt ips from ipv6_subnet
-    podnet_a = f'{ipv6_subnet.split("/")[0]}10:0:2'
-    podnet_b = f'{ipv6_subnet.split("/")[0]}10:0:3'
-
-    data_dict = {}
-
-    data_dict[podnet_a] = {
-        'entry': None,
-    }
-
-    data_dict[podnet_b] = {
-        'entry': None,
-    }
-
-    # Get `podnet_a_enabled` and `podnet_b_enabled`
-    podnet_a_enabled = config.get('podnet_a_enabled', None)
-    if podnet_a_enabled is None:
-        retval = False
-        message_list.append(messages[3214])
-    podnet_b_enabled = config.get('podnet_b_enabled', None)
-    if podnet_a_enabled is None:
-        retval = False
-        message_list.append(messages[3215])
-
-    # First run on enabled PodNet
-    if podnet_a_enabled is True and podnet_b_enabled is False:
-        enabled = podnet_a
-        disabled = podnet_b
-    elif podnet_a_enabled is False and podnet_b_enabled is True:
-        enabled = podnet_b
-        disabled = podnet_a
-    elif podnet_a_enabled is True and podnet_b_enabled is True:
-        retval = False
-        message_list.append(messages[3216])
-    elif podnet_a_enabled is False and podnet_b_enabled is False:
-        retval = False
-        message_list.append(messages[3217])
-    else:
-        message_list.append(messages[3218])
-
-    if retval == False:
-        return retval, data_dict, message_list
+    status, config_data, msg = load_pod_config(config_file)
+    if not status:
+      if config_data['raw'] is None:
+          return False, msg
+      else:
+          return False, msg + "\nJSON dump of raw configuration:\n" + json.dumps(config_data['raw'],
+              indent=2,
+              sort_keys=True)
+    enabled = config_data['processed']['enabled']
+    disabled = config_data['processed']['enabled']
 
     name_grepsafe = name.replace('.', '\.')
 
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from typing import Any, Dict, Tuple
 # libs
 from jinja2 import Environment, meta, FileSystemLoader, Template
 # local
-from .exceptions import (
+from exceptions import (
     CouldNotFindPodNets,
     InvalidPodNetPrivate,
     InvalidPodNetMgmt,
@@ -53,18 +53,87 @@ def check_template_data(template_data: Dict[str, Any], template: Template) -> Tu
     return success, err
 
 
-def load_pod_config(config_filepath: str) -> Dict[str, Any]:
+def load_pod_config(config_file=None, prefix=4000) -> Dict[str, Any]:
     """
-    Checks for pod config.json from supplied config_filepath or the current working directory and
-    Loads into a json object and return the object
+    Checks for pod config.json from supplied config_filepath or the current working directory, 
+    loads into a json object and returns the object
     :return data: object with podnet config
     """
-    file_path = Path(config_filepath)
-    if not file_path.exists():
-        raise FileNotFoundError
-    with file_path.open('r') as json_file:
-        return json.load(json_file)
 
+    messages = {
+      10: 'Config file {config_file} loaded.',
+      11: 'Failed to open {config_file}: ',
+      12: 'Failed to parse {config_file}: ',
+      13: 'Failed to get `ipv6_subnet from config_file.',
+      14: 'Invalid value for `ipv6_subnet` from config file {config_file}',
+      15: 'Failed to get `podnet_a_enabled` from config file {config_file}',
+      16: 'Failed to get `podnet_b_enabled` from config file {config_file}',
+      17: 'Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, both are True',
+      18: 'Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, both are False',
+      19: 'Invalid values for `podnet_a_enabled` and `podnet_b_enabled`, one or both are non booleans',
+    }
+
+    # Default config_file if it is None
+    if config_file is None:
+        config_file = '/opt/robot/config.json'
+
+
+    config_data = {
+      'raw': None,
+      'processed': None
+    }
+
+    # Load config from config_file
+    try:
+        with Path(config_file).open('r') as file:
+            config['file'] = json.load(file)
+    except OSError as e:
+            return False, config_data, messages[prefix + 11] + e.__str__()
+    except Exception as e:
+            return False, config_data, messages[prefix + 12] + e.__str__()
+
+    config_data['raw'] = config
+
+    # Get the ipv6_subnet from config_file
+    ipv6_subnet = config.get('ipv6_subnet', None)
+    if ipv6_subnet is None:
+        return False, config_data, ("%d: " % prefix + 13) + messages[13]
+    # Verify the ipv6_subnet value
+    try:
+        ipaddress.ip_network(ipv6_subnet)
+    except ValueError:
+        return False, config_data, messages[14]
+
+    # Get the PodNet Mgmt ips from ipv6_subnet
+    config['processed']['podnet_a'] = f'{ipv6_subnet.split("/")[0]}10:0:2'
+    config['processed']['podnet_b'] = f'{ipv6_subnet.split("/")[0]}10:0:3'
+
+    # Get `podnet_a_enabled` and `podnet_b_enabled`
+    podnet_a_enabled = config.get('podnet_a_enabled', None)
+    if podnet_a_enabled is None:
+        return False, config_data, ("%d: " % prefix + 15) + messages[15]
+    podnet_b_enabled = config.get('podnet_b_enabled', None)
+    if podnet_a_enabled is None:
+        return False, config_data, ("%d: " % prefix + 16) + messages[16]
+
+    # Determine enabled and disabled PodNet
+    if podnet_a_enabled is True and podnet_b_enabled is False:
+        enabled = podnet_a
+        disabled = podnet_b
+    elif podnet_a_enabled is False and podnet_b_enabled is True:
+        enabled = podnet_b
+        disabled = podnet_a
+    elif podnet_a_enabled is True and podnet_b_enabled is True:
+        return False, config_data, ("%d: " % prefix + 17) + messages[17]
+    elif podnet_a_enabled is False and podnet_b_enabled is False:
+        return False, config_data, ("%d: " % prefix + 18) + messages[18]
+    else:
+        return False, config_data, ("%d: " % prefix + 19) + messages[19]
+
+    config_data['processed']['enabled'] = enabled
+    config_data['processed']['disabled'] = disabled
+
+    return True, config_data, ("%d: " % prefix + 10) + messages[10]
 
 def get_podnets(config_filepath):
     data = load_pod_config(config_filepath)
