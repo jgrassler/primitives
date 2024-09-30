7c5db0c6074e6a21f63f4da58a5104545fc6a130
Johannes Grassler <jgr-github@btw23.de>
2024-09-30 11:06:00 +0200

    Update cloudcix_primitives/directorymain.py
    Co-authored-by: Maria Walsh <mariawalsh86@hotmail.com>

--- cloudcix_primitives/directorymain.py
+++ cloudcix_primitives/directorymain.py
@@ -269,7 +269,7 @@ def scrub(path: str, config_file=None) -> Tuple[bool, str]:
         if ret["channel_code"] != CHANNEL_SUCCESS:
             return False, fmt.channel_error(ret, f"{prefix+1}: " + messages[prefix+1]), fmt.successful_payloads
         if ret["payload_code"] != SUCCESS_CODE:
-            return False, fmt.payload_error(ret, f"{prefix+3}: " + messages[prefix+3]), fmt.successful_payloads
+            return False, fmt.payload_error(ret, f"{prefix+2}: " + messages[prefix+2]), fmt.successful_payloads
         fmt.add_successful('delete_directory', ret)
 
         return True, "", fmt.successful_payloads
