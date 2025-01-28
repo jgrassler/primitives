1f686a548e4b0f9ca9c6af519b5de02f54acdbdb
Johannes Grassler <info@computer-grassler.de>
2025-01-28 17:33:22 +0000

    default_firewall_ns: add scrub() verb.

--- cloudcix_primitives/default_firewall_ns.py
+++ cloudcix_primitives/default_firewall_ns.py
@@ -353,8 +353,95 @@ def build(
 
 
 def read() -> Tuple[bool, dict, str]:
-    return(False, {}, 'Not Implemted')
+    return(False, {}, 'Not Implemented')
 
 
-def scrub() -> Tuple[bool, str]:
-    return(False, 'Not Implemted')
+def scrub(
+    namespace: str,
+    config_file=None
+    ) -> Tuple[bool, str]:
+    """
+    description:
+        Builds nftables tables, base chains, user chains, interface sets, jump
+        rules and default base chain rules required for a CloudCIX project.
+        Throughout the life time of a project, the base chains are never
+        modified by another primitive. The user chains are managed by user
+        chain primitives such as project_firewall_ns or nat_firewall_ns.
+
+    parameters:
+        namespace:
+            description: VRF network name space's identifier, such as 'VRF453
+            type: string
+            required: true
+        public_bridge:
+          description: the name space's public bridge interface's name, such as 'br-B1'.
+          type: string
+          required: true
+        config_file:
+            description: path to the config.json file
+            type: string
+            required: false
+    return:
+        description: |
+            A tuple with a boolean flag stating if the build was successful or not and
+            the output or error message.
+        type: tuple
+    """
+
+    messages = {
+    1000: f'1000: Successfully scrubbed default firewall in project name space {namespace} on both PodNet nodes.',
+
+    3121: f'Failed to connect to the enabled PodNet for flush_ruleset payload: ',
+    3122: f'Failed to run flush_ruleset payload on the enabled PodNet. Payload exited with status ',
+    3161: f'Failed to connect to the disabled PodNet for flush_ruleset payload: ',
+    3162: f'Failed to run flush_ruleset payload on the disabled PodNet. Payload exited with status ',
+    }
+
+    # Default config_file if it is None
+    if config_file is None:
+        config_file = '/opt/robot/config.json'
+
+    status, config_data, msg = load_pod_config(config_file)
+    if not status:
+      if config_data['raw'] is None:
+          return False, msg
+      else:
+          return False, msg + "\nJSON dump of raw configuration:\n" + json.dumps(config_data['raw'],
+              indent=2,
+              sort_keys=True)
+    enabled = config_data['processed']['enabled']
+    disabled = config_data['processed']['disabled']
+
+    def run_podnet(podnet_node, prefix, successful_payloads):
+        rcc = SSHCommsWrapper(comms_ssh, podnet_node, 'robot')
+        fmt = PodnetErrorFormatter(
+            config_file,
+            podnet_node,
+            podnet_node == enabled,
+            {'payload_message': 'STDOUT', 'payload_error': 'STDERR'},
+            successful_payloads
+        )
+
+        payloads = {
+            'flush_ruleset': f'ip netns exec {namespace} nft flush ruleset'
+                                'nft add chain inet FILTER %(chain_name)s',
+        }
+
+        ret = rcc.run(payloads['flush_ruleset'])
+        if ret["channel_code"] != CHANNEL_SUCCESS:
+            return False, fmt.channel_error(ret, f"{prefix+1}: " + messages[prefix+1]), fmt.successful_payloads
+        if ret["payload_code"] != SUCCESS_CODE:
+            return False, fmt.payload_error(ret, f"{prefix+2}: " + messages[prefix+2]), fmt.successful_payloads
+        fmt.add_successful('flush_ruleset', ret)
+        return True, "", fmt.successful_payloads
+
+
+    status, msg, successful_payloads = run_podnet(enabled, 3120, {})
+    if status == False:
+        return status, msg
+
+    status, msg, successful_payloads = run_podnet(disabled, 3160, successful_payloads)
+    if status == False:
+        return status, msg
+
+    return True, messages[1000]
