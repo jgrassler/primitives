8dfc0ccad367e5b726753efd4d564afbf2d8ea12
Johannes Grassler <info@computer-grassler.de>
2025-01-29 15:46:52 +0000

    nat_firewall_ns: implement scrub verb.

--- cloudcix_primitives/nat_firewall_ns.py
+++ cloudcix_primitives/nat_firewall_ns.py
@@ -24,11 +24,7 @@ def build(
 ) -> Tuple[bool, str]:
     """
     description:
-        Builds nftables tables, base chains, user chains, interface sets, jump
-        rules and default base chain rules required for a CloudCIX project.
-        Throughout the life time of a project, the base chains are never
-        modified by another primitive. The user chains are managed by user
-        chain primitives such as project_firewall_ns or nat_firewall_ns.
+        Creates 1:1 and network NAT rules in a name space.
 
     parameters:
         namespace:
@@ -66,7 +62,7 @@ def build(
     """
 
     messages = {
-    1000: f'1000: Successfully created default firewall in project name space {namespace} on both PodNet nodes.',
+    1000: f'1000: Successfully created NAT rules in project name space {namespace} on both PodNet nodes.',
 
     3021: f'Failed to connect to the enabled PodNet for flush_postrouting payload: ',
     3022: f'Failed to run flush_postrouting payload on the enabled PodNet. Payload exited with status ',
@@ -198,20 +194,31 @@ def scrub(
     ) -> Tuple[bool, str]:
     """
     description:
-        Builds nftables tables, base chains, user chains, interface sets, jump
-        rules and default base chain rules required for a CloudCIX project.
-        Throughout the life time of a project, the base chains are never
-        modified by another primitive. The user chains are managed by user
-        chain primitives such as project_firewall_ns or nat_firewall_ns.
+        Flushes all 1:1 and network NAT rules in a name space.
 
     parameters:
         namespace:
             description: VRF network name space's identifier, such as 'VRF453
             type: string
             required: true
-        public_bridge:
-          description: the name space's public bridge interface's name, such as 'br-B1'.
-          type: string
+        one_to_one:
+          description: List of 1:1 mapping dictonaries from private IP address to public IP address. May be empty.
+          type: list
+          properties:
+            private:
+                description:
+                    The private IP address to map to a public IP address.
+            public:
+                description:
+                    The public IP address a private IP address is mapped to.
+          required: true
+        ranges
+          description: flat list of IP addresses to map to the name space's public IP address. May be empty.
+          type: list
+          required: true
+        public_ip_ns:
+          description: the name space's pulic IP address.
+          type: str
           required: true
         config_file:
             description: path to the config.json file
@@ -225,17 +232,23 @@ def scrub(
     """
 
     messages = {
-    1000: f'1000: Successfully scrubbed default firewall in project name space {namespace} on both PodNet nodes.',
+    1000: f'1000: Successfully flushed NAT rules in project name space {namespace} on both PodNet nodes.',
 
-    3121: f'Failed to connect to the enabled PodNet for flush_ruleset payload: ',
-    3122: f'Failed to run flush_ruleset payload on the enabled PodNet. Payload exited with status ',
-    3161: f'Failed to connect to the disabled PodNet for flush_ruleset payload: ',
-    3162: f'Failed to run flush_ruleset payload on the disabled PodNet. Payload exited with status ',
+    3021: f'Failed to connect to the enabled PodNet for flush_postrouting payload: ',
+    3022: f'Failed to run flush_postrouting payload on the enabled PodNet. Payload exited with status ',
+    3023: f'Failed to connect to the enabled PodNet for flush_prerouting payload: ',
+    3024: f'Failed to run flush_prerouting payload on the enabled PodNet. Payload exited with status ',
+
+    3061: f'Failed to connect to the enabled PodNet for flush_postrouting payload: ',
+    3062: f'Failed to run flush_postrouting payload on the enabled PodNet. Payload exited with status ',
+    3063: f'Failed to connect to the enabled PodNet for flush_prerouting payload: ',
+    3064: f'Failed to run flush_prerouting payload on the enabled PodNet. Payload exited with status ',
     }
 
     # Default config_file if it is None
     if config_file is None:
         config_file = '/opt/robot/config.json'
+
 
     status, config_data, msg = load_pod_config(config_file)
     if not status:
@@ -259,23 +272,42 @@ def scrub(
         )
 
         payloads = {
-            'flush_ruleset': f'ip netns exec {namespace} nft flush ruleset'
+            'flush_postrouting': f'ip netns exec ns1100 nft flush inet NAT POSTROUTING',
+            'flush_prerouting': f'ip netns exec ns1100 nft flush inet NAT PREROUTING',
         }
 
-        ret = rcc.run(payloads['flush_ruleset'])
+
+        rule_templates = {
+          'prerouting_11': f'ip netns exec {namespace} '
+                           'nft add rule ip NAT POSTROUTING ip saddr %(private)s snat to %(public)s',
+          'postrouting_11': f'ip netns exec {namespace} '
+                            'nft add rule ip NAT PREROUTING ip saddr %(private)s snat to %(public)s',
+          'range': f'ip netns exec {namespace} '
+                   f'nft add rule ip NAT POSTROUTING ip saddr %(network)s snat to {public_ip_ns}'
+        }
+
+        ret = rcc.run(payloads['flush_postrouting'])
         if ret["channel_code"] != CHANNEL_SUCCESS:
             return False, fmt.channel_error(ret, f"{prefix+1}: " + messages[prefix+1]), fmt.successful_payloads
         if ret["payload_code"] != SUCCESS_CODE:
             return False, fmt.payload_error(ret, f"{prefix+2}: " + messages[prefix+2]), fmt.successful_payloads
-        fmt.add_successful('flush_ruleset', ret)
+        fmt.add_successful('flush_postrouting', ret)
+
+        ret = rcc.run(payloads['flush_prerouting'])
+        if ret["channel_code"] != CHANNEL_SUCCESS:
+            return False, fmt.channel_error(ret, f"{prefix+3}: " + messages[prefix+3]), fmt.successful_payloads
+        if ret["payload_code"] != SUCCESS_CODE:
+            return False, fmt.payload_error(ret, f"{prefix+4}: " + messages[prefix+4]), fmt.successful_payloads
+        fmt.add_successful('flush_prerouting', ret)
+
         return True, "", fmt.successful_payloads
 
 
-    status, msg, successful_payloads = run_podnet(enabled, 3120, {})
+    status, msg, successful_payloads = run_podnet(enabled, 3020, {})
     if status == False:
         return status, msg
 
-    status, msg, successful_payloads = run_podnet(disabled, 3160, successful_payloads)
+    status, msg, successful_payloads = run_podnet(disabled, 3060, successful_payloads)
     if status == False:
         return status, msg
 
